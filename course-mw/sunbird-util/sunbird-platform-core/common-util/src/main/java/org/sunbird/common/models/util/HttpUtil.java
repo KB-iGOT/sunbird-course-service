71366129c7979bc8308eeb0cd36651f8d07490bb
sahilchaudhary <sahil.chaudhary@tarento.com>
2024-10-19 11:41:31 +0530

    adding the logs

--- course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/HttpUtil.java
+++ course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/HttpUtil.java
@@ -86,7 +86,6 @@ public class HttpUtil {
     logger.info(null,"The request url is :" + requestURL + ": headers : " + headers + " : params : " + params);
     HttpResponse<String> httpResponse = Unirest.post(requestURL).headers(headers).body(params).asString();
     String str = httpResponse.getBody();
-    logger.info(null,"The response of the request :" + str + " for params is: " + params);
     long stopTime = System.currentTimeMillis();
     long elapsedTime = stopTime - startTime;
     logger.info( null,
