23af2ba9d7439522405bf13f5fc89001208df54b
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2025-04-01 11:16:55 +0530

    Constants added

--- course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/event/impl/EventEnrolmentDaoImpl.java
+++ course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/event/impl/EventEnrolmentDaoImpl.java
@@ -406,10 +406,10 @@ public class EventEnrolmentDaoImpl implements EventEnrolmentDao {
         if (StringUtils.isNotEmpty(startDateStr) && StringUtils.isNotEmpty(endDateStr)) {
             LocalDate startDate = LocalDate.parse(startDateStr);
             LocalDate endDate = LocalDate.parse(endDateStr);
-                if ((startDate.isBefore(LocalDate.parse((String) contentDetails.get("startDate")))
-                        || startDate.isEqual(LocalDate.parse((String) contentDetails.get("startDate"))))
-                        && (endDate.isAfter(LocalDate.parse((String) contentDetails.get("endDate")))
-                        || endDate.isEqual(LocalDate.parse((String) contentDetails.get("endDate"))))) {
+                if ((startDate.isBefore(LocalDate.parse((String) contentDetails.get(JsonKey.START_DATE)))
+                        || startDate.isEqual(LocalDate.parse((String) contentDetails.get(JsonKey.START_DATE))))
+                        && (endDate.isAfter(LocalDate.parse((String) contentDetails.get(JsonKey.END_DATE)))
+                        || endDate.isEqual(LocalDate.parse((String) contentDetails.get(JsonKey.END_DATE))))) {
                 addEventDetailsToEnrollment(
                         request,
                         enrollment,
