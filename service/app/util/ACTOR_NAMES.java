b8e99391d911b3055a302513650953e2062f47ac
pathiknandi <pathik.nandi@tarento.com>
2025-07-10 16:32:18 +0530

    bug fixed for missing actor name

--- service/app/util/ACTOR_NAMES.java
+++ service/app/util/ACTOR_NAMES.java
@@ -55,7 +55,8 @@ public enum ACTOR_NAMES {
   EVENT_BATCH_CERTIFICATE_ACTOR(
       EventBatchCertificateActor.class, "event-batch-certificate-actor"),
   EXTENDED_COURSE_ENROLMENT_ACTOR(ExtendedCourseEnrollmentActor.class, "extended-course-enrolment-actor"),
-  COURSE_ENROLMENT_ACTOR_V3(CourseEnrolmentActorV3.class, "course-enrolment-actor-v3");
+  COURSE_ENROLMENT_ACTOR_V3(CourseEnrolmentActorV3.class, "course-enrolment-actor-v3"),
+  EXTENDED_CONTENT_CONSUMPTION_ACTOR(ExtendedContentConsumptionActor.class, "extended-content-consumption-actor");
 
 
   private ACTOR_NAMES(Class clazz, String name) {
