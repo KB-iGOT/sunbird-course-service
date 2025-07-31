eb48cd8f47b1a4cab8f9c6c40570566ae6d54145
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-07-31 18:40:45 +0530

    Revert "updated the isBaseLang key (#182)"
    This reverts commit 64f1d3656f21f85b8f542fbbf323f29cc03cd322.

--- course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/event/EventManagementActor.java
+++ course-mw/course-actors-common/src/main/java/org/sunbird/learner/actors/event/EventManagementActor.java
@@ -281,13 +281,11 @@ public class EventManagementActor extends BaseActor {
             int hoursSpentOnCourses = 0;
             String lrcProgressDetails = (String) eventDetails.get(JsonKey.LRC_PROGRESS_DETAILS);
             try {
-                if (StringUtils.isNotBlank(lrcProgressDetails) && isCertificateExist) {
-                    JsonNode lrcProgressDetailsJson = mapper.readTree(lrcProgressDetails);
-                    if (lrcProgressDetailsJson.hasNonNull(JsonKey.DURATION)) {
-                        String durationValue = lrcProgressDetailsJson.get(JsonKey.DURATION).asText();
-                        int duration = parseDurationValue(durationValue);
-                        hoursSpentOnCourses += duration;
-                    }
+                JsonNode lrcProgressDetailsJson = mapper.readTree(lrcProgressDetails);
+                if (lrcProgressDetailsJson != null && lrcProgressDetailsJson.hasNonNull(JsonKey.DURATION) && isCertificateExist) {
+                    String durationValue=lrcProgressDetailsJson.get(JsonKey.DURATION).asText();
+                    int duration = parseDurationValue(durationValue);
+                    hoursSpentOnCourses += duration;
                 }
             } catch (Exception e) {
                 logger.error(request.getRequestContext(), "Error parsing progressDetails JSON", e);
