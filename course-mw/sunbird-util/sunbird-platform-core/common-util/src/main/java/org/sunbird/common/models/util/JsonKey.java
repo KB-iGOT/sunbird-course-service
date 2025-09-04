22e4daf3ec67224db78b927ccfcefb29178f849c
anilkumarkammalapalli <anilkumar.kammalapalli@tarento.com>
2025-09-04 21:11:12 +0530

    Fix primaryCategory check for blended programs: allow Learning Resource

--- course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -1272,6 +1272,7 @@ public final class JsonKey {
   public static final String RECORDS = "records";
   public static final String CONTENT_READ = "content_read";
   public static final String DATE_KEY = "Date";
+    public static final String LEARNING_RESOURCE = "Learning Resource";
 
   private JsonKey() {}
 }
