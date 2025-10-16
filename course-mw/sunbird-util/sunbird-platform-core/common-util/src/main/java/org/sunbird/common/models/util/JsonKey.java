3c05785e3f82a7160e69f08d1434786e179a5b66
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2025-10-16 19:08:23 +0530

    added pr 288

--- course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
+++ course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/models/util/JsonKey.java
@@ -1282,6 +1282,8 @@ public final class JsonKey {
   public static final String CONTENT_READ_REQUIRED_FIELDS = "content_read_required_fields";
   public static final String INSTRUCTORS_USER_ID = "instructorsUserId";
   public static final String BATCH_ATTRIBUTES = "batchAttributes";
+  public static final String COURSE_CONTENT_ALLOWED_FIELDS = "course_content_allowed_fields";
+  public static final String CONTENT_ATTRIBUTES = "contentAttributes";
 
   private JsonKey() {}
 }
