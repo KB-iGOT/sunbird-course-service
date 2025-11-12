c5181b87acca4a3bcf5459e218dbbb2c2670ecf0
tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
2025-11-12 15:11:11 +0530

    Updated the Error message for the enrollment of the events.

--- course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/responsecode/ResponseMessage.java
+++ course-mw/sunbird-util/sunbird-platform-core/common-util/src/main/java/org/sunbird/common/responsecode/ResponseMessage.java
@@ -487,7 +487,7 @@ public interface ResponseMessage {
     String PRE_ENROLLMENT_RESOURCE_NOT_COMPLETED = "Pre-Enrollments Criterias are enabled for the course but not completed by the user.";
     String BATCH_SIZE_EXCEEDED_VALUE = "Max batch size exceeded. Only {0} more users can be enrolled to this batch.";
     String BATCH_SIZE_NOT_DEFINED_VALUE = "Batch size is not defined for this course batch.";
-    String MINIMUM_ENROLLMENT_CRITERIA_NOT_MET_VALUE = "Minimum enrollment criteria not met for this Event.";
+    String MINIMUM_ENROLLMENT_CRITERIA_NOT_MET_VALUE = "You need to complete at least 30% of the associated course before enrolling for this event";
     String ZERO_OR_MORE_THAN_ONE_ACTIVE_BATCH_VALUE = "User is enrolled to zero or more than one active batch for this course.";
   }
 
